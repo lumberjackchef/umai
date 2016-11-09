785a75a67512a859cec4750cc08bb36c5fc04e21
Ryan Pearson <ryan@ryanpearson.net>
2016-11-09 14:45:50 -0800

    prettifies the output logs

--- logger.go
+++ logger.go
@@ -1,23 +1,38 @@
 package main
 
 import (
+  "fmt"
   "log"
   "net/http"
   "time"
+
+  "github.com/fatih/color"
 )
 
 func Logger(inner http.Handler, name string) http.Handler {
   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    start := time.Now()
+    l := log.New(
+      new(logWriter),
+      "",
+      log.Lshortfile,
+    )
+
+    method := color.New(color.FgCyan).SprintFunc()
 
     inner.ServeHTTP(w, r)
 
-    log.Printf(
-      "%s\t%s\t%s\t%s", // make this prettier
-      r.Method,
+    l.Printf(
+      "%s\t%s\t",
+      method(r.Method),
       r.RequestURI,
-      name,
-      time.Since(start),
     )
   })
+}
+
+type logWriter struct {}
+
+func (writer logWriter) Write(bytes []byte) (int, error) {
+  timey := color.New(color.FgHiMagenta).SprintFunc()
+
+  return fmt.Print(timey(time.Now().UTC().Format("2006-01-02 15:04:05 MST ")) + "| " + string(bytes))
 }
